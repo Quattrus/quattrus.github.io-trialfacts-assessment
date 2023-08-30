e5c8a930c3e462ac9dd14e5cf1d7b33956dde15e
Romeo IV <94739286+Quattrus@users.noreply.github.com>
2023-08-30 16:32:27 +0800

    adjustments

--- opnstrtmap.js
+++ opnstrtmap.js
@@ -1,11 +1,7 @@
-// Initialize the map
-var map = L.map('map').setView([-33.88162, 151.04883], 15); // Cumberland Campus coordinates and zoom level
-
-// Add a base layer (OpenStreetMap tiles)
+var map = L.map('map').setView([-33.88162, 151.04883], 15); 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-// Add a marker for the address
 var marker = L.marker([-33.88162, 151.04883]).addTo(map);
 marker.bindPopup("Cumberland Campus, The University of Sydney, 75 East St. Lidcombe, NSW");
